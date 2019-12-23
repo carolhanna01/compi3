1fe04a3374b040467460911083650e6641c69db4
Your Name <you@example.com>
2019-12-24 00:34:02 +0200

    mergeeeeeeeee

--- scopes.h
+++ scopes.h
@@ -63,6 +63,7 @@ namespace scopeTables{
             vector<enumEntry> enums;
             bool insideLoop;
             functionEntry* currentFunc;
+            Scope* parent;
 
 
             // Constructor
